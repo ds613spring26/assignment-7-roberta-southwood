3ba7b5ef9d1a0980c3f03de48f4bbf7eedf13271
Roberta Southwood <trixie.southwood@gmail.com>
2026-03-29 21:16:04 -0400

    Added buissiness logic

--- Southwood_Assignment_7_app.R
+++ Southwood_Assignment_7_app.R
@@ -1,23 +1,6 @@
 # Roberta Southwood 
 # Copy of App.r 
 
-# Global variables can go here
-n <- 200
+# 1. Business Logic (runs once)
+library(shiny) 
 
-
-# Define the UI
-ui <- bootstrapPage(
-  numericInput('n', 'Number of obs', n),
-  plotOutput('plot')
-)
-
-
-# Define the server code
-server <- function(input, output) {
-  output$plot <- renderPlot({
-    hist(runif(input$n))
-  })
-}
-
-# Return a Shiny app object
-shinyApp(ui = ui, server = server)

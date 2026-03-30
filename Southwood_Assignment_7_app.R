706bef329745df0d1ddb40dcd78be14f35999006
Roberta Southwood <trixie.southwood@gmail.com>
2026-03-29 21:21:25 -0400

    Finished UI

--- Southwood_Assignment_7_app.R
+++ Southwood_Assignment_7_app.R
@@ -1,6 +1,28 @@
 # Roberta Southwood 
 # Copy of App.r 
 
-# 1. Business Logic (runs once)
+# Business Logic (runs once)
 library(shiny) 
+
+# Define UI
+
+ui <- bootstrapPage(
+  
+  titlePanel("Roberta Southwood Assignment 7"),
+  
+  # original app code 
+  numericInput('n', 'Number of obs', 200),
+  
+  # select hist or boxplot 
+  selectInput("plotType", "Choose plot type:", choices = c("Histogram", "Boxplot")),
+  
+  # color for boxplot 
+  conditionalPanel(
+    condition = "input.plotType == Boxplot",
+    selectInput("color", "Chppse boxplot color:", choices = c("gray", "red", "orange", "yellow", "green", "blue", "purple"))),
+  
+  # orignal app code
+  plotOutput('plot')
+  
+  )
 
